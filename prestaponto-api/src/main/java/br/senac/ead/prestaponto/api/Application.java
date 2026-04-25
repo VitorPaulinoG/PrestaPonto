23acac8d4082fe506b4cad833642eab1fcd379c8
Vitor Paulino <vitorpaulinog@gmail.com>
2026-04-25 15:26:42 -0300

    add: renomeando classe de inicialização

--- prestaponto-api/src/main/java/br/senac/ead/prestaponto/api/Application.java
+++ prestaponto-api/src/main/java/br/senac/ead/prestaponto/api/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PrestapontoApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PrestapontoApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
