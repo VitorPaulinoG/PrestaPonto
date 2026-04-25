b18ff79b86c9df1f9391b7a0fe60d02136b1e7f3
Vitor Paulino <vitorpaulinog@gmail.com>
2026-04-25 14:16:10 -0300

    add: bootstrap básico da API Rest Spring

---
+++ prestaponto-api/src/main/java/br/senac/ead/prestaponto/api/PrestapontoApiApplication.java
@@ -0,0 +1,13 @@
+package br.senac.ead.prestaponto.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestapontoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestapontoApiApplication.class, args);
+	}
+
+}
---
+++ prestaponto-api/src/test/java/br/senac/ead/prestaponto/api/PrestapontoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.ead.prestaponto.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestapontoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
