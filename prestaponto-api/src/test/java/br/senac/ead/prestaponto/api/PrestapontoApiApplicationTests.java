5a577947c70ed50115787336147b7e52924d92ea
Vitor Paulino <vitorpaulinog@gmail.com>
2026-04-25 15:18:37 -0300

    add: configuração de propriedades da aplicação e envs

--- prestaponto-api/src/test/java/br/senac/ead/prestaponto/api/PrestapontoApiApplicationTests.java
+++ prestaponto-api/src/test/java/br/senac/ead/prestaponto/api/PrestapontoApiApplicationTests.java
@@ -2,8 +2,10 @@ package br.senac.ead.prestaponto.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PrestapontoApiApplicationTests {
 
 	@Test
