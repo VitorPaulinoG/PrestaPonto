86d925ecc9d147ead354997fdd9151d091fd83ac
Vitor Paulino <vitorpaulinog@gmail.com>
2026-05-16 10:37:59 -0300

    remove: select de tipo de usuário na login page

--- prestaponto-ui/src/app/features/auth/pages/login-page/login-page.component.ts
+++ prestaponto-ui/src/app/features/auth/pages/login-page/login-page.component.ts
@@ -2,29 +2,24 @@ import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { AuthShellComponent } from '../../components/auth-shell/auth-shell.component';
-import { AUTH_ROLE_OPTIONS, AuthRole } from '../../models/auth.models';
 import { Button } from "../../../../shared/components/button/button";
 import { TextField } from "../../../../shared/components/text-field/text-field";
-import { SelectField } from "../../../../shared/components/select-field/select-field";
 
 type LoginFormGroup = FormGroup<{
-  role: FormControl<AuthRole>;
   email: FormControl<string>;
   password: FormControl<string>;
 }>;
 
 @Component({
   selector: 'app-login-page',
-  imports: [ReactiveFormsModule, AuthShellComponent, Button, TextField, SelectField],
+  imports: [ReactiveFormsModule, AuthShellComponent, Button, TextField],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
   private readonly formBuilder = inject(NonNullableFormBuilder);
 
-  protected readonly roleOptions = AUTH_ROLE_OPTIONS;
   protected readonly form: LoginFormGroup = this.formBuilder.group({
-    role: this.formBuilder.control<AuthRole>('PROVIDER', Validators.required),
     email: this.formBuilder.control('', [Validators.required, Validators.email]),
     password: this.formBuilder.control('', [Validators.required]),
   });
