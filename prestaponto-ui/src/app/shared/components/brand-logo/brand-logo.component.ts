da4891fb97cd9f48b4bdc6f2b10c43eaf764c883
Vitor Paulino <vitorpaulinog@gmail.com>
2026-05-17 13:12:10 -0300

    add: estilos tailwind ao component brand-logo

--- prestaponto-ui/src/app/shared/components/brand-logo/brand-logo.component.ts
+++ prestaponto-ui/src/app/shared/components/brand-logo/brand-logo.component.ts
@@ -3,7 +3,6 @@ import { Component, input } from '@angular/core';
 @Component({
   selector: 'app-brand-logo',
   templateUrl: './brand-logo.component.html',
-  styleUrl: './brand-logo.component.scss',
 })
 export class BrandLogoComponent {
   readonly inverted = input(false);
